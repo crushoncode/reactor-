9b1a8657a89598a248b0d405f4e83fa97333668f
Serina Ko <serinako08@gmail.com>
2018-11-12 17:13:52 +1100

    pass params to the route it is navigating to

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -3,27 +3,54 @@ import { View, Text, Button } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Home'
+  };
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
         <Button
           title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
+          onPress={() =>
+            this.props.navigation.navigate('Details', {
+              itemId: 86,
+              otherParam: 'anything you want here'
+            })
+          }
         />
       </View>
     );
   }
 }
 
+// Pass params to a route by putting them in an object as a second parameter to the navigation.
+// navigate function: this.props.navigation.navigate('RouteName', { /* params go here */ })
+
+// Read the params in your screen component: this.props.navigation.getParam(paramName, defaultValue).
+
 class DetailsScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('otherParam', 'A nested Details Screen')
+    };
+  };
   render() {
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherParam = navigation.getParam('otherParam', 'some default value');
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Detail Screen</Text>
+        <Text>itemId: {JSON.stringify(otherParam)}</Text>
         <Button
           title="Go to Details...again"
-          onPress={() => this.props.navigation.push('Details')}
+          onPress={() =>
+            this.props.navigation.push('Details', {
+              itemId: Math.floor(Math.random() * 100)
+            })
+          }
         />
       </View>
     );
