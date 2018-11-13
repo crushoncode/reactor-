84048495c79077638a8a7c791b11d02c2f8f21ab
Serina Ko <serinako08@gmail.com>
2018-11-13 12:02:08 +1100

    create a full-screen modal

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text, Button, Image } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { View, Text, Button } from 'react-native';
+import LogoTitle from './LogoTitle';
 
-class LogoTitle extends Component {
-  render() {
-    return (
-      <Image
-        source={require('../assets/images/spiro.png')}
-        style={{ width: 30, height: 30 }}
-      />
-    );
-  }
-}
-
-class HomeScreen extends Component {
+export default class HomeScreen extends Component {
   static navigationOptions = ({ navigation }) => {
+    const params = navigation.state.params || {};
     return {
       headerTitle: <LogoTitle />,
-      headerRight: (
+      headerLeft: (
         <Button
-          onPress={navigation.getParam('increaseCount')}
-          title="+1"
-          color="#fff"
+          onPress={() => navigation.navigate('MyModal')}
+          title="Info"
+          color="#ffffff"
         />
+      ),
+      headerRight: (
+        <Button onPress={params.increaseCount} title="+1" color="ffffff" />
       )
     };
   };
 
   componentDidMount() {
+    // setParams: make changes to route's params
     this.props.navigation.setParams({ increaseCount: this._increseCount });
   }
 
@@ -43,6 +37,7 @@ class HomeScreen extends Component {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Text>Count: {this.state.count}</Text>
         <Button
           title="Go to Details"
           onPress={() => {
@@ -56,80 +51,3 @@ class HomeScreen extends Component {
     );
   }
 }
-
-// Pass params to a route by putting them in an object as a second parameter to the navigation.
-// navigate function: this.props.navigation.navigate('RouteName', { /* params go here */ })
-
-// Read the params in your screen component: this.props.navigation.getParam(paramName, defaultValue).
-
-// In order to use params in the title, we need to make navigationOptions a function that returns a configuration object.
-
-class DetailsScreen extends Component {
-  static navigationOptions = ({ navigation, navigationOptions }) => {
-    const { params } = navigation.state;
-    return {
-      title: params ? params.otherParam : 'A nested Details Screen',
-      //   title: navigation.getParam('otherParam', 'A nested Details Screen')
-      headerStyle: {
-        backgroundColor: navigationOptions.headerTintColor
-      },
-      headerTintColor: navigationOptions.headerStyle.backgroundColor
-    };
-  };
-  render() {
-    const { navigation } = this.props;
-    const itemId = navigation.getParam('itemId', 'NO-ID');
-    const otherParam = navigation.getParam('otherParam', 'some default value');
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
-        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() =>
-            this.props.navigation.push('Details', {
-              itemId: Math.floor(Math.random() * 100)
-            })
-          }
-        />
-        // It's often necessary to update the navigationOptions configuration
-        for the active screen from the mounted screen component itself.
-        <Button
-          title="Update the title"
-          onPress={() =>
-            this.props.navigation.setParams({ otherParam: 'Updated!' })
-          }
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
-  }
-}
-
-export default createStackNavigator(
-  {
-    Home: HomeScreen,
-    Details: DetailsScreen
-  },
-  {
-    initialRouteName: 'Home',
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: '#f4511e'
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold'
-      }
-    }
-  }
-);
